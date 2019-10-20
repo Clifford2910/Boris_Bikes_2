2db7f06fcde6cce3e80deb54250326e0db62a5b8
Student <student@makerss-mbp.home>
2019-10-20 14:20:25 +0100

    working on step 13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,26 +1,22 @@
 class DockingStation
-
- attr_reader :docked_bikes
-
+  attr_reader :docked_bikes
   def release_bike
     raise "no bikes available" unless @docked_bikes
     @docked_bikes
   end
 
   def dock(bike)
+    raise "too many bikes docked" if @docked_bikes
     @docked_bikes = bike
   end
 
   def show_docked_bikes
     @docked_bikes
   end
-
 end
 
 class Bike
-
   def working?
     true
   end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,6 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   describe '#release_bike' do
-
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error("no bikes available")
     end
@@ -15,7 +14,6 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
-
   end
 
   it 'checks bike works' do
@@ -25,18 +23,27 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'docks a bike' do
-    bike1 = Bike.new
-    subject.dock(bike1)
-    expect(subject.dock(bike1)).to eq bike1
+  describe '#dock' do
+    it 'raises an error when there are too many bikes docked' do
+      bike = Bike.new
+      subject.dock(bike)
+      bike1 = Bike.new
+      expect { subject.dock(bike1) }.to raise_error("too many bikes docked")
+    end
+
+    it 'docks a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.dock(bike)).to eq @docked_bikes
+    end
   end
 
   it { is_expected.to respond_to :show_docked_bikes }
 
   it 'shows docked bikes' do
-    bike2 = Bike.new
-    subject.dock(bike2)
-    expect(subject.show_docked_bikes).to eq bike2
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.show_docked_bikes).to eq bike
   end
 
 end
